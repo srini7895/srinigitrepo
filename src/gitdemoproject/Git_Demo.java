944112b4b91aecaeac30c6a41513615bf3a53507
srini <srini@DESKTOP-2FFAMKB>
2023-10-14 21:16:37 +0530

    first commit

---
+++ src/gitdemoproject/Git_Demo.java
@@ -0,0 +1,20 @@
+package gitdemoproject;
+
+public class Git_Demo {
+	
+	public static String name = "srini";
+
+	public static void main(String[] args) {
+		if(name=="srini") {
+		System.out.println("he is a human");
+		}else {
+			System.out.println("he is not a human");
+		}
+		
+		 
+		
+	}
+	
+	
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author srini
+ *
+ */
+module gitdemoproject {
+}
